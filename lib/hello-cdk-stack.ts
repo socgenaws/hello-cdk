e3db279dc0df92fc75b88c5bd128d84a0fce8b24
ravi <ravi@vytec.com>
2024-01-16 12:30:07 +0530

    added

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -82,7 +82,7 @@ export class HelloCdkStack extends cdk.Stack {
 
     const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
       vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.MICRO),
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
       machineImage: ec2.MachineImage.genericLinux({
         'us-east-1': 'ami-06a8a766f09436b30',
       })
