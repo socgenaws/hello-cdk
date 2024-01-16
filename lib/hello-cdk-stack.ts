9b0dfc26ffe2439d98d903f905dc6ba6485dbc13
ravi <ravi@vytec.com>
2024-01-16 10:17:31 +0530

    added cdk with vpc

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
+
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -8,5 +10,39 @@ export class HelloCdkStack extends cdk.Stack {
     new s3.Bucket(this, 'MyFirstBucketdchgdcghfcd1r610120241240', {
       versioned: true
     });
+
+    // 👇 create VPC in which we'll launch the Instance
+    const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 0,
+      subnetConfiguration: [
+        {name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC},
+      ],
+    });
+
+    // 👇 create Security Group for the Instance
+    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+      vpc,
+      allowAllOutbound: true,
+    });
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    webserverSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+    
   }
 }
