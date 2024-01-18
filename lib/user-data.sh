47c9ed1729ea5bc77150136b7d0870b05a7ff526
ravi <ravi@vytec.com>
2024-01-18 10:49:45 +0530

    added

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -2,6 +2,18 @@
 yum update -y
 sudo su
 
+### Initial steps to mount the volume  ###
+
+mkfs -t xfs /dev/xvdf
+yum install xfsprogs -y
+mkdir /wddProjects
+mount /dev/xvdf /wddProjects
+
+### On Server reboot re-attach volume 
+sudo cp /etc/fstab /etc/fstab.orig
+blkid | egrep "/dev/xvdf: UUID="
+echo "UUID=xxx  /wddProjects  xfs  defaults,nofail  0  2" >> /etc/fstab
+
 amazon-linux-extras install -y nginx1
 systemctl start nginx
 systemctl enable nginx
