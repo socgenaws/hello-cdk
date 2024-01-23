05d1dc55ab05db27aa2237a4c2d9d0b61c9d0f61
ravi <ravi@vytec.com>
2024-01-23 11:57:50 +0530

    added

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -8,18 +8,28 @@ export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'vpc', {natGateways: 1});
+    // Create a VPC
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2, // Specify the desired number of Availability Zones
+    });
 
-    const alb = new elbv2.ApplicationLoadBalancer(this, 'alb', {
+    // Create a Network Load Balancer
+    const nlb = new elbv2.NetworkLoadBalancer(this, 'MyNlb', {
       vpc,
-      internetFacing: true,
+      internetFacing: true, // Set to true if you want it to be internet-facing
     });
 
-    const listener = alb.addListener('Listener', {
-      port: 80,
-      open: true,
+    // Create a listener for the NLB
+    const listener = nlb.addListener('MyListener', {
+      port: 80, // Set the desired port
     });
 
+      // Create an Auto Scaling Group
+      // const asg = new autoscaling.AutoScalingGroup(this, 'MyAsg', {
+      //   vpc,
+      //   instanceType: new ec2.InstanceType('t2.micro'), // Choose your instance type
+      //   machineImage: new ec2.AmazonLinuxImage(), // Use Amazon Linux as the AMI
+      // });
     const userData = ec2.UserData.forLinux();
     userData.addCommands(
       'sudo su',
@@ -30,49 +40,22 @@ export class HelloCdkStack extends cdk.Stack {
     );
 
     const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
-      vpc,
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.BURSTABLE2,
-        ec2.InstanceSize.MICRO,
-      ),
-      machineImage: new ec2.AmazonLinuxImage({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      }),
-      userData,
-      minCapacity: 2,
-      maxCapacity: 3,
-    });
-
-    listener.addTargets('default-target', {
-      port: 80,
+        vpc,
+        instanceType: ec2.InstanceType.of(
+          ec2.InstanceClass.BURSTABLE2,
+          ec2.InstanceSize.MICRO,
+        ),
+        machineImage: new ec2.AmazonLinuxImage({
+          generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+        }),
+        userData,
+        minCapacity: 2,
+        maxCapacity: 3,
+      });
+    // Add the ASG as the target to the NLB listener
+    listener.addTargets('AsgTarget', {
       targets: [asg],
-      healthCheck: {
-        path: '/',
-        unhealthyThresholdCount: 2,
-        healthyThresholdCount: 5,
-        interval: cdk.Duration.seconds(30),
-      },
-    });
-
-    listener.addAction('/static', {
-      priority: 5,
-      conditions: [elbv2.ListenerCondition.pathPatterns(['/static'])],
-      action: elbv2.ListenerAction.fixedResponse(200, {
-        contentType: 'text/html',
-        messageBody: '<h1>Static ALB Response</h1>',
-      }),
-    });
-
-    asg.scaleOnRequestCount('requests-per-minute', {
-      targetRequestsPerMinute: 60,
-    });
-
-    asg.scaleOnCpuUtilization('cpu-util-scaling', {
-      targetUtilizationPercent: 75,
-    });
-
-    new cdk.CfnOutput(this, 'albDNS', {
-      value: alb.loadBalancerDnsName,
+      port: 80, // Set the port that the ASG instances are listening on
     });
   }
 }
