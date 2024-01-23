e3c947174833871a8be493e77dc8970820dab019
ravi <ravi@vytec.com>
2024-01-23 12:16:48 +0530

    added

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -25,33 +25,33 @@ export class HelloCdkStack extends cdk.Stack {
     });
 
       // Create an Auto Scaling Group
-      // const asg = new autoscaling.AutoScalingGroup(this, 'MyAsg', {
-      //   vpc,
-      //   instanceType: new ec2.InstanceType('t2.micro'), // Choose your instance type
-      //   machineImage: new ec2.AmazonLinuxImage(), // Use Amazon Linux as the AMI
-      // });
-    const userData = ec2.UserData.forLinux();
-    userData.addCommands(
-      'sudo su',
-      'yum install -y httpd',
-      'systemctl start httpd',
-      'systemctl enable httpd',
-      'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html',
-    );
-
-    const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
+      const asg = new autoscaling.AutoScalingGroup(this, 'MyAsg', {
         vpc,
-        instanceType: ec2.InstanceType.of(
-          ec2.InstanceClass.BURSTABLE2,
-          ec2.InstanceSize.MICRO,
-        ),
-        machineImage: new ec2.AmazonLinuxImage({
-          generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-        }),
-        userData,
-        minCapacity: 2,
-        maxCapacity: 3,
+        instanceType: new ec2.InstanceType('t2.micro'), // Choose your instance type
+        machineImage: new ec2.AmazonLinuxImage(), // Use Amazon Linux as the AMI
       });
+    // const userData = ec2.UserData.forLinux();
+    // userData.addCommands(
+    //   'sudo su',
+    //   'yum install -y httpd',
+    //   'systemctl start httpd',
+    //   'systemctl enable httpd',
+    //   'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html',
+    // );
+
+    // const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
+    //     vpc,
+    //     instanceType: ec2.InstanceType.of(
+    //       ec2.InstanceClass.BURSTABLE2,
+    //       ec2.InstanceSize.MICRO,
+    //     ),
+    //     machineImage: new ec2.AmazonLinuxImage({
+    //       generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+    //     }),
+    //     userData,
+    //     minCapacity: 2,
+    //     maxCapacity: 3,
+    //   });
     // Add the ASG as the target to the NLB listener
     listener.addTargets('AsgTarget', {
       targets: [asg],
