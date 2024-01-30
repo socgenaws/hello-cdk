35157e34419439679bfb0e15353def8cb1cd8302
ravi <ravi@vytec.com>
2024-01-30 11:45:53 +0530

    added

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,24 +1,36 @@
-import * as s3 from 'aws-cdk-lib/aws-s3';
+// import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib';
-import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
+import {BucketDeployment, Source} from "@aws-cdk/aws-s3-deployment";
+import {Bucket, BucketAccessControl} from "@aws-cdk/aws-s3";
+import * as path from "path";
+
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myBucket = new s3.Bucket(this, 'MyTempFileBucketdddd250120241250', {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      transferAcceleration: true,
-    });    
-    const deployment = new s3Deployment.BucketDeployment(
-      this,
-      'deployStaticWebsite',
-      {
-        sources: [s3Deployment.Source.asset('../website')],
-        destinationBucket: myBucket,
-      }
-    );
+    const bucket = new Bucket(this, 'MyTempFileBucketdddd250120241250', {
+      accessControl: BucketAccessControl.PRIVATE,
+    })
+
+    new BucketDeployment(this, 'BucketDeployment', {
+      destinationBucket: bucket,
+      sources: [Source.asset(path.resolve(__dirname, '../website'))]
+    })
+
+    // const myBucket = new s3.Bucket(this, 'MyTempFileBucketdddd250120241250', {
+    //   removalPolicy: cdk.RemovalPolicy.DESTROY,
+    //   autoDeleteObjects: true,
+    //   transferAcceleration: true,
+    // });    
+    // const deployment = new s3Deployment.BucketDeployment(
+    //   this,
+    //   'deployStaticWebsite',
+    //   {
+    //     sources: [s3Deployment.Source.asset('../website')],
+    //     destinationBucket: myBucket,
+    //   }
+    // );
   }
 }
 
