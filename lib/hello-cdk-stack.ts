96e93bad8706f2e9c34ea24eba2f6d389f37b8d4
ravi <ravi@vytec.com>
2024-01-30 10:03:08 +0530

    updated s3

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,17 +1,41 @@
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib';
 
-
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'MyTempFileBucket250120241250', {
+    const s3Bucket = new s3.Bucket(this, 's3-bucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
-      transferAcceleration: true,
+      // 👇 set up lifecycle rules
+      lifecycleRules: [
+        {
+          // 👇 optionally apply object name filtering
+          // prefix: 'data/',
+          abortIncompleteMultipartUploadAfter: cdk.Duration.days(90),
+          expiration: cdk.Duration.days(365),
+          transitions: [
+            {
+              storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+              transitionAfter: cdk.Duration.days(30),
+            },
+            {
+              storageClass: s3.StorageClass.INTELLIGENT_TIERING,
+              transitionAfter: cdk.Duration.days(60),
+            },
+            {
+              storageClass: s3.StorageClass.GLACIER,
+              transitionAfter: cdk.Duration.days(90),
+            },
+            {
+              storageClass: s3.StorageClass.DEEP_ARCHIVE,
+              transitionAfter: cdk.Duration.days(180),
+            },
+          ],
+        },
+      ],
     });
-    
-    bucket.transferAccelerationUrlForObject('abc.txt');
+    s3Bucket.transferAccelerationUrlForObject('abc.txt');
   }
 }
