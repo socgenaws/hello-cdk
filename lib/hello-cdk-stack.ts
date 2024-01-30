fc5e1965dcc932e350c71055cff905670cdbf16a
ravi <ravi@vytec.com>
2024-01-30 12:41:11 +0530

    added

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -4,6 +4,7 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { RemovalPolicy} from 'aws-cdk-lib';
 import * as path from 'path';
+import * as iam from 'aws-cdk/aws-iam';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -12,11 +13,15 @@ export class HelloCdkStack extends cdk.Stack {
     // Content bucket
     const siteBucket = new s3.Bucket(this, 'SiteBucket', {
       bucketName: 'mytempfilebucketdddd250120241250',
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: true,
       removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
       autoDeleteObjects: true, // NOT recommended for production code
     });
+    const result = siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [siteBucket.arnForObjects('index.html')],
+      principals: [new iam.AccountRootPrincipal()],
+    }));
 
     // Deploy site contents to S3 bucket
     new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
